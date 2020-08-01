c7ab9949faffb58a02858f9ec1a70f2a1e8a684e
Adam Spice <adwasere@gmail.com>
2020-08-02 11:09:28 +1200

    added night vision

---
+++ Interface/VisionMode.gd
@@ -0,0 +1,33 @@
+extends CanvasModulate
+
+const DARK = Color("1b1a1a")
+const NIGTHVISION = Color("37bf62")
+
+var cooldown = false
+
+func _ready():
+	visible = true
+	color = DARK
+
+func cycle_vision_mode():
+	if not cooldown:
+		if color == NIGTHVISION:
+			DARK_mode()
+		else:
+			NIGTHVISION_mode()
+			cooldown = true
+			$Timer.start()
+				
+func DARK_mode():
+	color = DARK
+	$AudioStreamPlayer2D.stream = load("res://SFX/nightvision_off.wav")
+	$AudioStreamPlayer2D.play()
+
+func NIGTHVISION_mode():
+	color = NIGTHVISION
+	$AudioStreamPlayer2D.stream = load("res://SFX/nightvision.wav")
+	$AudioStreamPlayer2D.play()
+
+
+func _on_Timer_timeout():
+	cooldown = false
