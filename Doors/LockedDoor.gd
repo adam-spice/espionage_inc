d366b6aeee88d0c4682055a8bd37b7fd2d5287f9
Adam Spice <adwasere@gmail.com>
2020-08-03 10:05:48 +1200

    Added auto generated combinations

--- Doors/LockedDoor.gd
+++ Doors/LockedDoor.gd
@@ -1,5 +1,9 @@
 extends "res://Doors/Door.gd"
 
+func _ready():
+	generate_combination()
+
+
 func _on_Door_input_event(_viewport, _event, _shape_idx):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
 		$CanvasLayer/NumPad.popup_centered()
@@ -13,3 +17,14 @@ func _on_Door_body_exited(body):
 func _on_NumPad_combination_correct():
 	open()
 	$CanvasLayer/NumPad.hide()
+	
+func generate_combination():
+	var length = 8
+	var combination = CombinationGenerator.generate_combination(length)
+	$CanvasLayer/NumPad.combination = combination
+	print(str(combination))
+	
+	
+	
+	
+	
