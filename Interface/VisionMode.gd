602461615c79c46d31db25c181ae6857debfe190
Adam Spice <adwasere@gmail.com>
2020-08-03 11:10:43 +1200

    hide torches and show labels in night vision mode

--- Interface/VisionMode.gd
+++ Interface/VisionMode.gd
@@ -22,11 +22,15 @@ func DARK_mode():
 	color = DARK
 	$AudioStreamPlayer2D.stream = load("res://SFX/nightvision_off.wav")
 	$AudioStreamPlayer2D.play()
+	get_tree().call_group("lights", "show")
+	get_tree().call_group("labels", "hide")
 
 func NIGTHVISION_mode():
 	color = NIGTHVISION
 	$AudioStreamPlayer2D.stream = load("res://SFX/nightvision.wav")
 	$AudioStreamPlayer2D.play()
+	get_tree().call_group("lights", "hide")
+	get_tree().call_group("labels", "show")
 
 
 func _on_Timer_timeout():
